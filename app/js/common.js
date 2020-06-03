86bd59d7af75b0845e1a92583d94a2209db71f49
Davyd Tevzadze <pawtod20@gmail.com>
2020-06-04 00:07:50 +0300

    some changes

--- app/js/common.js
+++ app/js/common.js
@@ -1,47 +1,66 @@
 $(function() {
-	var owl = $('.carousel');
-	owl.owlCarousel({
-		items:1,
-	    loop:true,
-	    margin:0,
-	    dots:false,
-	    nav:false,
-	});
-	// Go to the next item
-	$('.customNextBtn').click(function() {
-	    owl.trigger('next.owl.carousel');
-	})
-	// Go to the previous item
-	$('.customPrevBtn').click(function() {
-	    // With optional speed parameter
-	    // Parameters has to be in square bracket '[]'
-	    owl.trigger('prev.owl.carousel', [300]);
-	})
+    var owl = $('.carousel');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: false,
+    });
+    // Go to the next item
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
 
-	var owl2 = $('.slidertwo');
-	owl2.owlCarousel({
-		items:1,
-	    loop:true,
-	    margin:0,
-	    dots:false,
-	    nav:false,
-	});
-	// Go to the next item
-	$('.customNextBtntwo').click(function() {
-	    owl2.trigger('next.owl.carousel');
-	})
-	// Go to the previous item
-	$('.customPrevBtntwo').click(function() {
-	    // With optional speed parameter
-	    // Parameters has to be in square bracket '[]'
-	    owl2.trigger('prev.owl.carousel', [300]);
-	})
+    var owl2 = $('.slidertwo');
+    owl2.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: false,
+    });
+    // Go to the next item
+    $('.customNextBtntwo').click(function() {
+        owl2.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtntwo').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl2.trigger('prev.owl.carousel', [300]);
+    })
 
-	$('.more-info').on('mouseenter', function () {
-		$(this).addClass('hovered')
-	});
-	$('.more-info').on('mouseleave', function () {
-		$(this).removeClass('hovered')
-	})
+    $('.more-info').on('mouseenter', function() {
+        $(this).addClass('hovered')
+    });
+    $('.more-info').on('mouseleave', function() {
+        $(this).removeClass('hovered')
+    })
+
+    var sidebar = document.querySelector('.sidebar')
+    var rightOperations = document.querySelector('.operations-right')
+    // var sidebarTitle = document.querySelector('.sidebar-title')
+    // console.log(sidebar)
+    this.onscroll = function() {
+        var domRect = rightOperations.getBoundingClientRect();
+        var spaceBelow = domRect.top
+        console.log(spaceBelow)
+
+        if (spaceBelow < 250) {
+            sidebar.classList.add('fixed');
+            // sidebarTitle.classList.add('fixed');
+        } else {
+            sidebar.classList.remove('fixed');
+            // sidebarTitle.classList.remove('fixed');
+        }
+    };
+
 
 });
